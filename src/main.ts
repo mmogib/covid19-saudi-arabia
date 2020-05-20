2d0351c1f3659f29d9403cedf3f5f43854ae20fc
Mohammed Alshahrani <mmogib@gmail.com>
2020-05-20 16:43:54 +0300

    initial one

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-
+import { join } from 'path';
+import favicon from 'serve-favicon';
 // local import
 import apiRouter from './routes/api';
 import {
@@ -17,6 +18,11 @@ app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(join(__dirname, '../public')));
+app.use(favicon(join(__dirname, '../public', 'favicon-32x32.png')));
+
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('tiny'));
 
@@ -24,7 +30,7 @@ app.listen(port, () => {
   console.log(`Listening on port ${port} ...`);
 });
 
-app.use('/api', jwtcheck, apiRouter);
+app.use('/api', apiRouter);
 app.use('/', (_req, _res, next) => {
   next(new HttpException(404, 'not found'));
 });
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,19 +1,32 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { URL } from "url";
+import fetch from "node-fetch";
 
 const router = Router();
+const sourceUrl = new URL(
+  "https://datasource.kapsarc.org/api/records/1.0/search/?dataset=saudi-arabia-coronavirus-disease-covid-19-situation&sort=-date&facet=cumulative&facet=indicator&facet=date&facet=event&facet=region&rows=1000&facet=city",
+);
 
-router.get('/', (_req, res) => {
-  res.json({
-    message: 'Api homepage.💕',
-  });
+router.get("/", async (_req, res) => {
+  const url = `${sourceUrl.toString()}&q=city=Total`; //&q=city=dahran
+  const response = await fetch(url);
+  const data = await response.json();
+  const flds = data.records
+    .map((v: any) => v.fields)
+    .filter(
+      (vv: any) =>
+        vv.indicator.toLowerCase() === "cases" &&
+        vv.daily_cumulative.toLowerCase() === "cumulative",
+    );
+  res.render("index");
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const {
     body: { name },
   } = req;
   res.json({
-    name: name || 'nothing',
+    name: name || "nothing",
   });
 });
 
